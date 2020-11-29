c7cfa709d10f34928fc6aebb337426d697e4ea5b
Николай Митин <nick.mitin@gmail.com>
2020-11-30 00:11:47 +0300

    Fixed path to config file and config read from env

--- config/emarsys.php
+++ config/emarsys.php
@@ -3,6 +3,6 @@
 #Obtain these credentials from your Emarsys Account Security Settings
 #https://help.emarsys.com/hc/en-us/articles/115004740329-your-account-security-settings#api-users
 return [
-    'username' => 'username',
-    'secret' => 'secret',
+    'username' => env('EMARSYS_USERNAME', ''),
+    'secret' => env('EMARSYS_SECRET', ''),
 ];
--- src/Providers/LaravelEmarsysServiceProvider.php
+++ src/Providers/LaravelEmarsysServiceProvider.php
@@ -9,14 +9,15 @@ class LaravelEmarsysServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../../../config/emarsys.php' => config_path('emarsys.php')
+            __DIR__ . '/../../config/emarsys.php' => config_path('emarsys.php')
         ]);
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../../../config/emarsys.php', config_path('emarsys.php')
+            __DIR__ . '/../../config/emarsys.php',
+            config_path('emarsys.php')
         );
     }
 }
